8a3bded84f24686b4c3079ebabd729cba398834e
Kyle Sexton <ks@mocker.org>
2013-08-09 11:19:53 -0500

    Initial commit

---
+++ darwin-keychain.el
@@ -0,0 +1,17 @@
+;;; darwin-keychain.el
+;; Found in random gist, putting on github so I can el-get it
+
+(defun chomp (str)
+  "Chomp leading and tailing whitespace from STR."
+  (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'" str)
+    (setq str (replace-match "" t t str))) str)
+
+(defun get-keychain-password (account-name)
+  "Gets `account` keychain password from OS X Keychain"
+  (chomp
+   (shell-command-to-string
+    (concatenate
+     'string
+     "security 2>&1 >/dev/null find-generic-password -ga "
+     account-name
+     "| sed 's/^password: \\\"\\(.*\\)\\\"/\\1/'"))))
